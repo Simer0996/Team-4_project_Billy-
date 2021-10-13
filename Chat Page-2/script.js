b0fe9db311dc216229cfc12cdd4e62d95210980b
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2021-10-13 02:07:12 -0700

    feat: add JS to chat page-2

--- Chat Page-2/script.js
+++ Chat Page-2/script.js
@@ -1,4 +1,40 @@
+// // //Load firebase database 
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBJd0zUZ_bPZ3NFAfUwt0NhlsDJMdwhftY",
+//     authDomain: "billy-langara.firebaseapp.com",
+//     projectId: "billy-langara",
+//     storageBucket: "billy-langara.appspot.com",
+//     messagingSenderId: "809749583925",
+//     appId: "1:809749583925:web:afe89756b5dcff438e797c",
+//     measurementId: "G-B85DSNX3C4"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const database = firebase.database();
+
+
+let input = document.getElementById("message");
+
+$(".btn-send").on('click', () => {
+    let messages = document.getElementById("messages");
+    let value = message.val().value;
+
+    let addDiv = document.createElement("div");
+    let addSpan = document.createElement("span");
+
+    let addP = document.createElement("p");
+    messages.innerHTML = value;
+
+    addDiv.appendChild(addSpan);
+    addDiv.appendChild(addP);
+    messages.appendChild(addDiv);
+
+})
+
+
 // Click back button redirect to chat page-1
 $('.icon-back').on('click', () => {
-    window.location.href='../Chat Page-1/index.html'
+    window.location.href = '../Chat Page-1/index.html'
 })
