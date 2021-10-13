41368e90eb7945969c9a9de576191ac754e896c3
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2021-10-13 02:09:01 -0700

    feat: Activate fire base database to chat page-2

--- Chat Page-2/script.js
+++ Chat Page-2/script.js
@@ -1,18 +1,18 @@
-// // //Load firebase database 
+// //Load firebase database 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBJd0zUZ_bPZ3NFAfUwt0NhlsDJMdwhftY",
-//     authDomain: "billy-langara.firebaseapp.com",
-//     projectId: "billy-langara",
-//     storageBucket: "billy-langara.appspot.com",
-//     messagingSenderId: "809749583925",
-//     appId: "1:809749583925:web:afe89756b5dcff438e797c",
-//     measurementId: "G-B85DSNX3C4"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyBJd0zUZ_bPZ3NFAfUwt0NhlsDJMdwhftY",
+    authDomain: "billy-langara.firebaseapp.com",
+    projectId: "billy-langara",
+    storageBucket: "billy-langara.appspot.com",
+    messagingSenderId: "809749583925",
+    appId: "1:809749583925:web:afe89756b5dcff438e797c",
+    measurementId: "G-B85DSNX3C4"
+};
 
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = firebase.database();
 
 
 let input = document.getElementById("message");
