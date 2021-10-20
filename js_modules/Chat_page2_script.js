b6da75bc7906e56b74a4490144ffa85feba1573e
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2021-10-20 12:16:59 -0700

    feat: add Variable to JS

--- js_modules/Chat_page2_script.js
+++ js_modules/Chat_page2_script.js
@@ -35,6 +35,6 @@ $(".btn-send").on('click', () => {
 
 
 // Click back button redirect to chat page-1
-$('.icon-back').on('click', () => {
+let chatPageRedirectTo1 = $('.icon-back').on('click', () => {
     window.location.href = '../Chat Page-1/index.html'
 })
