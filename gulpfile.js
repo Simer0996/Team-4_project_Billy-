0eee094aa4fabaa3916d433e7f526e738486c247
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2021-11-03 01:09:26 -0700

    feat: gulpfile.js fix

--- gulpfile.js
+++ gulpfile.js
@@ -23,3 +23,32 @@ async function includeHTML() {
 }
 
 exports.default = includeHTML;
+
+
+// const gulp = require('gulp');
+// const fileinclude = require('gulp-file-include');
+// ​
+// const paths = {
+//     script: {
+//         src: './',
+//         dest: './build'
+//     }
+// };
+// ​
+// async function includeHTML() {
+//     return gulp.src([
+//             '*.html',
+//             '*css',
+//             '*js',
+//             '!gulpfile.js',
+//             '!header.html', //ignore
+//             '!footer.html', //ignore
+//         ])
+//         .pipe(fileinclude({
+//             prefix: '@@',
+//             basepath: '@file'
+//         }))
+//         .pipe(gulp.dest(paths.script.dest));
+// }
+// ​
+// exports.default = includeHTML;
