10574b7ce52b87e9127c6928cb16e1b8120bcda8
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2021-11-02 19:38:30 -0700

    feat: delete unneccesary files

--- setting_payment_link-1/script.js
+++ setting_payment_link-1/script.js
@@ -1,12 +1,3 @@
-// $('#menuMen').on('click', () => {
-//     $('#mainImg').css('display', 'none');
-//     $('#menImg').css('display', 'block');
-//     $('#womenImg').css('display', 'none');
-//     $('.exploreBtn2').css('display', 'none');
-//     $('.exploreBtn').css('display', 'block');
-//     $('#arrow').css('display', 'block');
-// })
-
 $('.addNew').on('click', ()=>{
     console.log('hello');
     location.href='../setting_payment_link-2/index.html';
