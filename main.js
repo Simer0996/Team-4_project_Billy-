90e18060780f69a4643c672f1f4742fbe62ea3c6
Simer Singh <simersingh@Simers-MacBook-Air.local>
2021-11-13 17:58:13 -0800

    updating

--- main.js
+++ main.js
@@ -49,10 +49,27 @@ function getClass(list, cls) {
 (async () => {
     if (getClass(mainClass, 'homePage')) {
         console.log('this is Home page');
-        const {
-            default: arrayHome
-        } = await import('./Home.js');
-        arrayHome;
+
+        let recoveredString = localStorage.getItem('bill_array');
+console.log(recoveredString);
+let newArray = JSON.parse(recoveredString);
+console.log(newArray);
+
+ function arrayHome() {
+for (let i of newArray) {
+    let row1 = document.getElementById("myTable").insertRow(0)
+    let cell1 = row1.insertCell(0);
+    let cell2 = row1.insertCell(1);
+    cell1.innerHTML = `${i.Catagories}`
+    cell2.innerHTML = `${i.Amount}$`
+   
+}
+}
+        // const {
+        //     default: arrayHome
+        // } = await import('./Home.js');
+        // arrayHome;
+        arrayHome()
     }
 })();
 
